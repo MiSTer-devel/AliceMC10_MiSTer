8b0c83608847595aaede2da55c7da3f9a7fcb10e
pcornier <pier.cornier@gmail.com>
2020-10-21 09:15:57 +0200

    keyboard fix

--- rtl/keyboard.sv
+++ rtl/keyboard.sv
@@ -237,16 +237,17 @@ always @(posedge clk_sys) begin
 			8'h49 : keys[5][6] <= ~press_btn; // .>
 			8'h4a : keys[5][7] <= ~press_btn; // /?
 
-			8'h5a : keys[6][0] <= ~press_btn; // ENTER
+			8'h14 : keys[6][0] <= ~press_btn; // CTRL
 
-			8'h0d : keys[6][1] <= ~press_btn; // TAB (PC)    -> CLEAR (TRS)
+			//8'h0d : keys[6][1] <= ~press_btn; // TAB (PC)    -> CLEAR (TRS)
 			8'h76 : keys[6][2] <= ~press_btn; // ESCAPE (PC) -> BREAK (TRS)
 
-			8'h75 : keys[3][3] <= ~press_btn; // UP ARROW
-			8'h72 : keys[3][4] <= ~press_btn; // DN ARROW
-			8'h6B : keys[3][5] <= ~press_btn; // LF ARROW (PC)  -> LF ARROW (TRS)
-			8'h66 : keys[3][5] <= ~press_btn; // BACKSPACE (PC) -> LF ARROW (TRS)
-			8'h74 : keys[3][6] <= ~press_btn; // RT ARROW
+			//8'h75 : keys[3][3] <= ~press_btn; // UP ARROW
+			//8'h72 : keys[3][4] <= ~press_btn; // DN ARROW
+			//8'h6B : keys[3][5] <= ~press_btn; // LF ARROW (PC)  -> LF ARROW (TRS)
+			//8'h66 : keys[3][5] <= ~press_btn; // BACKSPACE (PC) -> LF ARROW (TRS)
+			//8'h74 : keys[3][6] <= ~press_btn; // RT ARROW
+			8'h5a : keys[3][6] <= ~press_btn; // ENTER
 			8'h29 : keys[3][7] <= ~press_btn; // SPACE
 
 
--- rtl/mc10.v
+++ rtl/mc10.v
@@ -26,9 +26,9 @@ wire [11:0] addr_bus = { U5_Y, U6_Y } | vdg_addr[11:0];
 wire [7:0] cpu_dout, ram1_dout, ram2_dout, U7_dout, ram_din;
 wire [7:0] ram_data_bus = ram1_dout | ram2_dout;
 wire [3:0] r4, g4, b4;
-wire [7:0] KR;
-wire [5:0] U14_out, kb_rows;
-wire shift_key;
+wire [7:0] KR, kb_rows;
+wire [5:0] U14_out;
+wire shift = kb_rows[6];
 reg [5:0] U8;
 
 assign red[7:4] = r4;
@@ -45,7 +45,7 @@ MC6803_gen2 U1(
   .irq(0),
   .nmi(exp_in[2]),
   .PORT_A_IN(),
-  .PORT_B_IN(),
+  .PORT_B_IN({ 2'b0, shift, 1'b0 }),
   .DATA_IN(rom_dout | U7_dout | U14_out),
   .PORT_A_OUT(KR),
   .PORT_B_OUT(),
@@ -63,7 +63,7 @@ rom_mc10 U3(
 );
 
 x74155 U4(
-  .C({ U18_qn1, E_CLK }),
+  .C({ ~E_CLK, E_CLK }),
   .G({ exp_in[0] | cpu_addr[12], exp_in[0] }),
   .A(cpu_addr[14]),
   .B(cpu_addr[15]),
@@ -138,7 +138,7 @@ mc6847_mc10 U11(
 );
 
 x14503B U14(
-  .in(kb_rows),
+  .in(~kb_rows),
   .out(U14_out),
   .A(U4_Y1[2] | ~cpu_rw),
   .B(U4_Y1[2] | ~cpu_rw)
@@ -150,19 +150,9 @@ keyboard keyboard(
   .ps2_key(ps2_key),
   .addr(KR),
   .kb_rows(kb_rows),
-  .kblayout(1'b1),
+  .kblayout(1'b0),
   .Fn(),
-  .modif(shift)
-);
-
-wire U18_qn1;
-x7474 U18(
-  .clk1(clk_35),
-  .p1(1'b0),
-  .c1(E_CLK),
-  .d1(U18_qn1),
-  .q1(),
-  .qn1(U18_qn1)
+  .modif()
 );
 
 endmodule
