585d655ebb15a1a89568d502ad061508368356f2
__bpardo <bernard.pardo@gmail.com>
2020-11-06 14:39:14 +0100

    Update k72c10.py
    Enhancements : the script can now have multiple files and the output is managed per file.

--- k72c10.py
+++ k72c10.py
@@ -1,31 +1,51 @@
 #!/usr/bin/python3.6
 
-import sys
+import sys, os
+from os.path import basename
 
-def main(argv):
+def convert(argv):
 
-    if len(argv) != 1:
+    print (argv)
+
+    if len(argv) == 0:
         sys.exit(2)
 
-    with open('k7.c10', 'wb') as c10:
+    for file in argv:
+        file_name,_ = os.path.splitext(file)
+        new_file=file_name+'.c10';
 
-        leader = bytearray([0x55] * 128)
-        ba = bytearray()
-        
-        with open(argv[0], 'rb') as k7:
-            ba.extend(leader)
-            k7.seek(8)
-            ba.extend(k7.read(3))
-            length = k7.read(1)
-            ba.extend(length)
-            ba.extend(k7.read(length[0]+1))
-            ba.extend(leader)
-            ba.extend(k7.read())
+        print(f"{file_name} is being process.")                
+     
+        with open(new_file, 'wb') as c10:
 
-        c10.write(ba)
-        c10.close()
+            leader = bytearray([0x55] * 128)
+            ba = bytearray()
+            
+            with open(argv[0], 'rb') as k7:
+                ba.extend(leader)
+                k7.seek(8)
+                ba.extend(k7.read(3))
+                length = k7.read(1)
+                ba.extend(length)
+                ba.extend(k7.read(length[0]+1))
+                ba.extend(leader)
+                ba.extend(k7.read())
+
+            c10.write(ba)
+            c10.close()
+        print(f"{new_file} is generated\n")
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    """
+        Input : 
+        the input can contain one or more parameter separated by space, these parameters can be 
+            - the name of a file
+           
+
+        Output : 
+            - filename with .c10 extension
+    """
+    convert(sys.argv[1:])
+
 
 
