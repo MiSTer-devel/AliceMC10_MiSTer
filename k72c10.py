3b454a5a66b0b9fa4ea832d885d551e50d2604b7
__bpardo <bernard.pardo@gmail.com>
2020-11-06 15:20:54 +0100

    Update k72c10.py
    Fix file process 
    Add the possibility to pass a file mask in input

--- k72c10.py
+++ k72c10.py
@@ -1,45 +1,43 @@
 #!/usr/bin/python3.6
 
-import sys, os
+import sys, os, glob
 from os.path import basename
 
 def convert(argv):
-
-    print (argv)
-
     if len(argv) == 0:
         sys.exit(2)
 
-    for file in argv:
-        file_name,_ = os.path.splitext(file)
-        new_file=file_name+'.c10';
+    for file_argument in argv:
+        for file in glob.glob(file_argument):        
+            file_name,_ = os.path.splitext(file)
+            new_file=file_name+'.c10';
 
-        print(f"{file_name} is being process.")                
-     
-        with open(new_file, 'wb') as c10:
+            print(f"{file_name} is being process.")                        
+            with open(new_file, 'wb') as c10:
 
-            leader = bytearray([0x55] * 128)
-            ba = bytearray()
-            
-            with open(argv[0], 'rb') as k7:
-                ba.extend(leader)
-                k7.seek(8)
-                ba.extend(k7.read(3))
-                length = k7.read(1)
-                ba.extend(length)
-                ba.extend(k7.read(length[0]+1))
-                ba.extend(leader)
-                ba.extend(k7.read())
+                leader = bytearray([0x55] * 128)
+                ba = bytearray()
+                
+                with open(file, 'rb') as k7:
+                    ba.extend(leader)
+                    k7.seek(8)
+                    ba.extend(k7.read(3))
+                    length = k7.read(1)
+                    ba.extend(length)
+                    ba.extend(k7.read(length[0]+1))
+                    ba.extend(leader)
+                    ba.extend(k7.read())
 
-            c10.write(ba)
-            c10.close()
-        print(f"{new_file} is generated\n")
+                c10.write(ba)
+                c10.close()
+            print(f"{new_file} is generated\n")
 
 if __name__ == '__main__':
     """
         Input : 
         the input can contain one or more parameter separated by space, these parameters can be 
             - the name of a file
+            - a file mask (*.k7) for example
            
 
         Output : 
